44bff5f5265eedd76ee8205ce39af2ffe3c57f58
Tarusben862 <bensontarus204@gmail.com>
2026-07-02 18:44:31 +0300

    Refactor RadioButtonDemo for better readability
    Refactored RadioButtonDemo class to improve structure and readability.

--- RadioButtonDemo.java
+++ RadioButtonDemo.java
@@ -1,100 +1,71 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Scanner;
 
-public class RadioButtonDemo extends JFrame implements ActionListener
-{
+public class RadioButtonDemo extends JFrame implements ActionListener {
+
     JRadioButton bird, cat, dog, rabbit, pig;
-    ButtonGroup pets;
-    JLabel picture;
+    JLabel display;
 
-    ImageIcon birdPic = new ImageIcon("bird.jpg");
-    ImageIcon catPic = new ImageIcon("cat.jpg");
-    ImageIcon dogPic = new ImageIcon("dog.jpg");
-    ImageIcon rabbitPic = new ImageIcon("rabbit.jpg");
-    ImageIcon pigPic = new ImageIcon("pig.jpg");
-
-    public RadioButtonDemo()
-    {
+    public RadioButtonDemo() {
+        
         setTitle("RadioButtonDemo");
-        setSize(600,350);
-        setLayout(new FlowLayout());
+        setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
 
+        
         bird = new JRadioButton("Bird");
         cat = new JRadioButton("Cat");
         dog = new JRadioButton("Dog");
         rabbit = new JRadioButton("Rabbit");
         pig = new JRadioButton("Pig");
 
-        pets = new ButtonGroup();
-
+        
+        ButtonGroup pets = new ButtonGroup();
         pets.add(bird);
         pets.add(cat);
         pets.add(dog);
         pets.add(rabbit);
         pets.add(pig);
 
+        
         bird.addActionListener(this);
         cat.addActionListener(this);
         dog.addActionListener(this);
         rabbit.addActionListener(this);
         pig.addActionListener(this);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5,1));
+        
+        display = new JLabel("Please select a pet");
 
-        panel.add(bird);
-        panel.add(cat);
-        panel.add(dog);
-        panel.add(rabbit);
-        panel.add(pig);
-
-        picture = new JLabel();
-
-        add(panel);
-        add(picture);
+       
+        add(bird);
+        add(cat);
+        add(dog);
+        add(rabbit);
+        add(pig);
+        add(display);
 
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e)
-    {
-        if(bird.isSelected())
-        {
-            picture.setIcon(birdPic);
-            JOptionPane.showMessageDialog(null,"You selected Bird");
-        }
+    
+    public void actionPerformed(ActionEvent e) {
+        String pet = e.getActionCommand();
 
-        if(cat.isSelected())
-        {
-            picture.setIcon(catPic);
-            JOptionPane.showMessageDialog(null,"You selected Cat");
-        }
+       
+        display.setText("Selected Pet: " + pet);
 
-        if(dog.isSelected())
-        {
-            picture.setIcon(dogPic);
-            JOptionPane.showMessageDialog(null,"You selected Dog");
-        }
-
-        if(rabbit.isSelected())
-        {
-            picture.setIcon(rabbitPic);
-            JOptionPane.showMessageDialog(null,"You selected Rabbit");
-        }
-
-        if(pig.isSelected())
-        {
-            picture.setIcon(pigPic);
-            JOptionPane.showMessageDialog(null,"You selected Pig");
-        }
+       
+        JOptionPane.showMessageDialog(this,
+                "You selected: " + pet,
+                "Pet Selection",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
-    public static void main(String[] args)
-    {
-        Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
         new RadioButtonDemo();
     }
 }
